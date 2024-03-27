005f22ca66151cde43258a0794d6fe2297e82365
Ashok <velmaashok765@gmail.com>
2024-03-27 13:27:07 +0000

    Soluton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+class App extends Component {
+  state = { ApiStatus: false, dataSets: [], dataLabels: [] };
+  componentDidMount() {
+    this.callApi();
+  }
+
+  callApi = async () => {
+    const options = {
+      method: "GET",
+    };
+    const response = await fetch(
+      "https://datausa.io/api/data?drilldowns=Nation&measures=Population",
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    const dataLabels = data.data.map((each) => each.Year);
+    const dataSets = data.data.map((each) => each.Population);
+    this.setState({ ApiStatus: true, dataSets, dataLabels });
+  };
+
+  getLoader = () => <div className="animate-loader"></div>;
+
+  getBarData = () => {
+    const { dataSets, dataLabels } = this.state;
+
+    return (
+      <div className="bar-chart">
+        <Bar
+          data={{
+            labels: dataLabels,
+            datasets: [
+              {
+                label: "",
+                data: dataSets,
+                backgroundColor: [
+                  "rgba(75,192,192,1)",
+                  "red",
+                  "green",
+                  "yellow",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+              },
+            ],
+          }}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "United States Population between 2013 to 2021",
+              },
+              responsive: true,
+              scales: {
+                yAxes: [
+                  {
+                    stacked: true,
+                    ticks: {
+                      beginAtZero: true,
+                    },
+                  },
+                ],
+              },
+            },
+          }}
+        />
+      </div>
+    );
+  };
+
+  render() {
+    const { ApiStatus } = this.state;
+
+    return (
+      <div className="bar-chart-con">
+        {ApiStatus === false ? this.getLoader() : this.getBarData()}
+      </div>
+    );
+  }
 }
 
 export default App;
